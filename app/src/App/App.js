706f03373d53cbe94fc6f11de17ce60afeb838b3
FSP\heddad <amine.heddad@fer-ge.ch>
2021-05-10 17:01:21 +0200

    Life cycle

--- app/src/App/App.js
+++ app/src/App/App.js
@@ -1,16 +1,31 @@
+import React from 'react';
 import './App.css';
 import Button from './components/Button/Button';
 
-function App() {
-  return (
-    <div className="App">
-      DEMAT BREIZH
-      <br/>
-      <Button bgcolor="tomato" title="Mon button"></Button>
-      <Button title="Default bgcolor"></Button>
-      <Button bgcolor="skyblue" title="My button2" style={{textDecoration: 'underline', color: 'grey'}} action={()=>{alert('Le bouton est cliqué')}}></Button>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {text: "Hello", counter:0}
+  }
+
+  componentDidMount() {
+    this.setState({text:'Cliquez ici !!'});
+  }
+
+  componentDidUpdate() {
+    console.log('Le contenu du component est: ', this.state);
+  }
+
+  render() {
+    return <div className="App">
+    <Button title="cliquer ici !!" action={()=>{
+      this.setState({counter:this.state.counter + 1})
+    }}/>
+    {JSON.stringify(this.state)}
     </div>
-  );
+  }
+
 }
 
 export default App;
